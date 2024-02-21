4def443e25387e15e6cc456e035c508e615e608d
moti126 <mordi1266@gmail.com>
2024-02-21 16:56:01 +0200

    this is my first lesson

---
+++ Netu4_lessons/targil1.py
@@ -0,0 +1,20 @@
+a = 12
+b = 11.56
+c = "mordi"
+
+# this is integer
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print("hello " + c + "\nHow are you? " + "\nyoure age is: " + str(a))
+
+
+mail=input("enter youre mail:")
+print(mail)
+print(type(mail))
